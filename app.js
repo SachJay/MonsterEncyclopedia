f3cabc8659bc449f778ab8b8831129a0bda84801
Sach <jayaweerasachin@gmail.com>
2019-09-02 17:08:49 -0400

    revamped InfoManager code to now load a new page when the info of a monster is being displayed

---
+++ app.js
@@ -0,0 +1,25 @@
+var express = require('express');
+const fs = require('fs');
+
+var app = express();
+var data;
+
+app.set('view engine', 'ejs');
+
+app.use('/assets', express.static('assets'));
+
+app.get('/', function(req, res){
+  res.render('homepage');
+});
+
+app.get('/', function(req, res){
+  res.render('homepage');
+});
+
+app.get('/monsters', function(req, res){
+  var contents = fs.readFileSync(__dirname + '/assets/data.json', 'utf8');
+  res.render('monsterInfo', {chara: JSON.parse(contents)[req.query.id]});
+});
+
+app.listen(3000, '127.0.0.1');
+console.log("Now listening to port 3000");
---
+++ assets/index.js
@@ -0,0 +1,90 @@
+var currentPage = null;
+var currentCatagory = null;
+var fileData;
+
+$(function(){
+  var $characters = $('#characters');
+
+  var charaTemplate = "" +
+  "<li>" +
+  "<p><strong>Name:</strong> {{name}}<p/>" +
+  "<p><strong>Hp:</strong> {{hit_points}}<p/>" +
+  "<button data-id='{{id}}' class='remove-chara'>X</button>" +
+  "</li>";
+
+  function addChara(chara){
+    $characters.append(Mustache.render(charaTemplate, chara));
+  }
+
+  $.ajax({
+    type: 'GET',
+    url: '/assets/data.json',
+    success: function(characters){
+      fileData = characters;
+      displayTopics();
+    }, error: function(){
+      alert("Error loading page");
+    }
+  });
+/*
+  $("#add-character").on("click", function(){
+    var chara = {
+      name: $name.val(),
+      hp: $hp.val()
+    };
+
+    $.ajax({
+      type: 'POST',
+      url: 'http://rest.learncode.academy/api/sachinthana/friends',
+      data: chara,
+      success: function(newChara){
+        addChara(newChara);
+      }
+    });
+  });
+
+  $characters.delegate('.remove-chara', 'click', function(){
+    var $li = $(this).closest('li');
+
+    $.ajax({
+      type: 'DELETE',
+      url: 'http://rest.learncode.academy/api/sachinthana/friends/' + $(this).attr('data-id'),
+      success: function(){
+        $li.remove();
+      }
+    });
+  });*/
+});
+
+function displayTopics(){
+  clearCurrentPage("everything", "currentCatagory");
+
+  var currentCatagory = document.createElement("div");
+  currentCatagory.id = "currentCatagory";
+  document.getElementById("everything").appendChild(currentCatagory);
+
+  for(var i = 0; i < fileData.length; i++){
+    createTopicButton(fileData[i], i);
+  }
+}
+
+function createTopicButton(topic, i){
+  var topicButton = document.createElement("input");
+  topicButton.type = "button";
+  topicButton.value = topic.name;
+  topicButton.addEventListener('click', function(){
+    $(location).attr('href', "http://127.0.0.1:3000/monsters?id="+i);
+    });
+  document.getElementById("currentCatagory").appendChild(topicButton);
+
+  var br = document.createElement("br");
+  document.getElementById("currentCatagory").appendChild(br);
+}
+
+function clearCurrentPage(parentId, childId){
+  document.getElementById(parentId).removeChild(document.getElementById(childId));
+}
+
+function getCatagory(catagoryNum){
+  return data[catagoryNum];
+}
